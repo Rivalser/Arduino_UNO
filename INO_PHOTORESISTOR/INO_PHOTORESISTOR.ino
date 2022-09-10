0aaef3491506aa9d03cefc5ad973571d37df71f7
Rivalser <97056846+Rivalser@users.noreply.github.com>
2022-09-10 21:40:14 +0200

    Create INO_PHOTORESISTOR.ino

---
+++ INO_PHOTORESISTOR/INO_PHOTORESISTOR.ino
@@ -0,0 +1,28 @@
+int lightPin = A0;
+int greenPin = 13;
+int redPin = 12;
+int lightVal;
+int delayT = 250;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(lightPin,INPUT);
+  pinMode(greenPin,OUTPUT);
+  pinMode(redPin,OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lightVal = analogRead(lightPin);
+  Serial.println(lightVal);
+  if(lightVal>20){
+    digitalWrite(greenPin,HIGH);
+    digitalWrite(redPin,LOW);
+  }else{
+    digitalWrite(greenPin,LOW);
+    digitalWrite(redPin,HIGH);
+  }
+//  delay(delayT);
+}
