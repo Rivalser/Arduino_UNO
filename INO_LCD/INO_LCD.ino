106c7c72c063a6c4fc864c719fda694a24e19e77
Rivalser <97056846+Rivalser@users.noreply.github.com>
2022-10-30 18:22:30 +0100

    Create INO_LCD.ino

---
+++ INO_LCD/INO_LCD.ino
@@ -0,0 +1,26 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();
+
+
+}
+
+
+void loop()
+{
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Just do it!");
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Rivalser Arduino!");
+  delay(2000);
+  lcd.clear();
+
+}
